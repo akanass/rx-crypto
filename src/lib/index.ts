3a7d77d6109080327a6fe36a1bcdb6bb58068429
akanass <akanass@icloud.com>
2019-08-27 12:58:06 +0200

    struct(rx-crypto): change library structure

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,6 @@
-export * from './services';
+export * from './random-string';
+export * from './AES';
+export * from './pem';
+export * from './rsa';
+export * from './hash';
+export * from './jwt';
--- src/lib/services/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './random-string';
-export * from './AES';
-export * from './pem';
-export * from './rsa';
-export * from './hash';
-export * from './jwt';
